f7080772a130debe4d15b6f1587392ad884689a1
rinkeshputtademo <rinkeshputta@outlook.com>
2025-03-23 17:36:30 +0530

    added the tf initial module code

--- providers.tf
+++ providers.tf
@@ -3,16 +3,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.10.0"
+      version = ">3.10.0"
     }
   }
 }
 
-
-provider "azurerm" {
-  features {}
-  client_id       = "24e2e249-13d6-4280-8791-2c2fba1c873e"
-  tenant_id       = "d870244a-9053-483e-9a46-0597932f0a73"
-  client_secret   = "UvV8Q~n_sQETtGryQyewWEtM3lJSnhGhFB2A3cg3"
-  subscription_id = "0f87b7f3-b8ff-4c25-b09a-901f6869202e"
-}
